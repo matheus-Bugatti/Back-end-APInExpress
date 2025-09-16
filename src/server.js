b4859a733378e0bd36dbc9dfcddc7860dc6c2bb2
matheus-Bugatti <campos.n@aluno.ifsp.edu.br>
2025-09-16 16:29:45 -0300

    AddAlt- Add cors e Middlewares e Alt schemaPrisma

--- src/server.js
+++ src/server.js
@@ -3,10 +3,14 @@ import profileRouter from './routers/profileRouter.js'
 import productRouter from './routers/productRouter.js'
 import supplierRouter from './routers/supplierRouter.js'
 import customerRouter from './routers/customerRouter.js'
+import cors from 'cors'
+import { logger } from './middlewares/logger.js'
 
 const app = express()
 const port = 3333
 
+app.use(logger)
+app.use(cors())//Gabilita o CORS para todas as rotas e origins
 app.use(express.json()) //converte os dados dos usuarios em json ou coisa assim
 
 app.use('/profile', profileRouter)
